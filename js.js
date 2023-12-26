92689f918bbc8253e63bf371c167d951264feac2
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-27 00:55:49 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,33 @@
+//obj1
+
+// let me = {
+//   firstName: "Abdulaziz",
+//   lastName: "Toshpulatov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+
+// const arrayToString = (arr) => arr.join('+');
+
+// const output = Object.entries(me)
+//   .map(([key, value]) => `${key}: ${Array.isArray(value) ? arrayToString(value, ",") : value}`)
+//   .join("\n");
+
+// console.log(output);
+
+//obj2
+
+// let n = 6;
+
+// for (let i = 1;i <= n; i++){
+//     let keys = i;
+//     let obj = {
+//         i : i,
+//         result: i * i,
+//     }
+//     console.log(obj);
+// }
+
+//obj3
+
