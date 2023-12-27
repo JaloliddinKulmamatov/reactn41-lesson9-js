8ea2e299ffa0244e64a614e4d5ea15c4c3cddaab
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-27 11:29:08 +0500

    jalol

--- js.js
+++ js.js
@@ -31,3 +31,42 @@
 
 //obj3
 
+// let mahsulot =  {
+//     name: "telefon",
+//     cost: 300,
+//     location: "Tashkent",
+// }
+
+// let keys = Object.keys(mahsulot);
+// let value = Object.values(mahsulot);
+
+// console.log("Keys:", ...keys)
+// console.log("Value:", ...value);
+
+//obj4
+
+// let strings = ["salom", "hayr", "qanday", "bugun", "yaxshi"];
+
+// let resultObject = {};
+
+// for (var i = 0; i < strings.length; i++) {
+//   let key = strings[i];
+//   let value = strings[i].length;
+//   resultObject[key] = value;
+// }
+
+// console.log(resultObject);
+
+
+//obj6
+
+let obj = { 
+    a:3,
+    b:2,
+    c:5,
+ }
+
+
+ console.log( ...obj);
+
+
